4d053cc997f4d41f4b8249c04ee1079f82b65de7
Antonios Sarikas <antonios.sarikas@gmail.com>
2023-12-31 00:43:38 +0200

    Update `README.md` and cleanup `rf.py`

--- rf.py
+++ rf.py
@@ -20,22 +20,22 @@ with open('data/MOFs/batch_train/clean_names.json', 'r') as fhand:
 with open('data/MOFs/batch_val_test/clean_names.json', 'r') as fhand:
     mof_test = json.load(fhand)['names'][5000:]
 
-column_names = [
-    'MOFname', 'CO2_uptake_P0.15bar_T298K [mmol/g]',
+# Define the features and the target.
+features = [
     'volume [A^3]', 'weight [u]', 'surface_area [m^2/g]',
     'void_fraction', 'void_volume [cm^3/g]', 'largest_free_sphere_diameter [A]',
     'largest_included_sphere_along_free_sphere_path_diameter [A]',
     'largest_included_sphere_diameter [A]',
     ]
 
+target = 'CO2_uptake_P0.15bar_T298K [mmol/g]'
+
 # Load features and labels.
-df = pd.read_csv('data/MOFs/all_MOFs_screening_data.csv')
-df.set_index('MOFname', inplace=True)
-features = column_names[2:]
-target = column_names[1]
+df = pd.read_csv('data/MOFs/all_MOFs_screening_data.csv', index_col='MOFname')
 
 reg = RandomForestRegressor(n_jobs=-1)
 
+# Drop missing values.
 df_test = df.loc[mof_test]
 df_test.replace([np.inf, -np.inf], np.nan, inplace=True)
 df_test.dropna(inplace=True)
@@ -59,7 +59,7 @@ for size in train_sizes:
 
     reg.fit(X_train, y_train)
 
-    print(size, r2_score(y_test, reg.predict(X_test)))
+    print(size, r2_score(y_true=y_test, y_pred=reg.predict(X_test)))
 
     # Save the trained model.
     #with open('rf_model.pkl', 'wb') as fhand:
